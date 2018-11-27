1d0c80114750d723d5198ad5e9c76858c1278456
Tuan Phan <tuanphan@ubuntu16.04>
2018-11-27 13:50:31 +1100

    make createOrderfunction idempotent based on orderId and version

--- createOrder.js
+++ createOrder.js
@@ -1,22 +1,23 @@
 import validate from './lib/validation';
 import { success, failure } from './lib/response';
 import { call } from './lib/dynamodb';
-import InvalidParrameterError from './lib/InvalidParrameterException';
+import InvalidParrameterError from './exception/InvalidParrameterException';
+import DuplicatedRequestException from './exception/DuplicatedRequestException';
 
-function buildRequestParameter(orderDetail) {
-  return orderDetail;
+function buildRequestParameter(orderDetails) {
+  return Object.assign({}, { TableName: process.env.ORDERS_TABLE }, { Item : orderDetails} , {  ConditionExpression: 'attribute_not_exists(orderId) AND attribute_not_exists(version)' } );
+  // return Object.assign({}, { TableName: process.env.ORDERS_TABLE }, Item);
 }
 
 export async function main(event, context) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
-
+  
   const orderDetail = {
     orderId: data.orderId,
     items: data.items,
-    version: Date.now(),
+    version: data.version,
   };
-
   // validate input
   const { errors, value } = validate(orderDetail, 'order');
   if (errors) {
@@ -24,21 +25,19 @@ export async function main(event, context) {
     throw new InvalidParrameterError('Invalid Order Details'.concat(errors.toString()));
     return;
   }
-
-  const Item = buildRequestParameter(value);
-  const params = {
-    TableName: process.env.ORDERS_TABLE,
-    // 'Item' contains the attributes of the item to be created
-    Item,
-  };
-
+  const params = buildRequestParameter(value);
+  console.log('DynamoDB called with: ',params)
   try {
     await call('put', params);
     return success(Object.assign({}, { item: params.Item }, { success: true }));
   } catch (e) {
     // retry behaviour is handled by aws sdk so we don't have to worry about it.
-    console.log(e);
-    throw e;
+    if (e.code === 'ConditionalCheckFailedException') {
+      throw new DuplicatedRequestException('Duplicated Order '.concat(data.orderId))
+    } else {
+      console.log('dynamodb put error: ',e);
+      throw e;  
+    }
   }
 }
 
---
+++ exception/DuplicatedRequestException.js
@@ -0,0 +1,5 @@
+export default class DuplicatedRequestException extends Error {
+    constructor(message) {
+      super(message);
+    }
+  }
