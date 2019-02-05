b3457ddf72c781cbd9fb8457f98ce0100ff6b610
Tuan Phan <phan.anh.tuan@gmail.com>
2019-02-06 10:26:56 +1100

    re-organise project structure

---
+++ katalon-test/Test Suites/RegressionTest.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d68b79dc-1e9b-411e-b82a-050fbb42900e</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb688579-9c2d-4b28-8308-10460fe845f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home Page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
